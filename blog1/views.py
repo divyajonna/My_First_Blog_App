f30ee27db9377bdd70f7a560fb59f6ef30f9bce6
Your Name <you@example.com>
2018-01-23 12:24:39 +0530

    added view post_list of our blog

--- blog1/views.py
+++ blog1/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.shortcuts import render
 
 # Create your views here.
+
+#creating post_list view
+def post_list(request):
+    return render(request, 'blog1/post_list.html',{})
+    #render (put together) our template blog/post_list.html.
